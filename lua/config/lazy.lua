c3cf47bd833cbb44163a4e1f1d7dcdacb8e482ab
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-03-14 19:06:12 -0600

    remove lazyVim default plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,26 +14,29 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Disable Netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Disable the LazyVim order check
+vim.g.lazyvim_check_order = false
+
+-- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
     { import = "plugins" },
+    { "folke/snacks.nvim", opts = {} },
   },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  -- automatically check for plugin updates
+  checker = { 
+    enabled = true 
   },
-  checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
@@ -50,4 +53,3 @@ require("lazy").setup({
     },
   },
 })
-require ('nvim-treesitter.install').compilers = { "zig", "gcc" }
