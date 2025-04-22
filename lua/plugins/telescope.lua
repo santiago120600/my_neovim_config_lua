517254848330761c03928c941846d487e9e66a80
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-04-22 17:35:12 -0600

    update telescope configuration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,28 +1,33 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    -- change some options
-    opts = {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = "Telescope",
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Live Grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Find Buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Help Tags" },
+    { "<leader>fo", "<cmd>Telescope oldfiles<cr>",   desc = "Recent Files" },
+    -- Add more mappings as needed (e.g., git_files, keymaps, commands)
+  },
+  config = function()
+    require("telescope").setup {
       defaults = {
         layout_strategy = "horizontal",
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
-        winblend = 0
-      },
-    },
-    mappings = {
-        n = {
-            ["<leader>ff"] = {
-                function()
-                    require("telescope.builtin").find_files()
-                end,
-                desc = "Find Files",
-            },
-            ["<leader>fb"] = {
-              function()
-                  require("telescope.builtin").buffers()
-              end,
-              desc = "Find buffer",
+        winblend = 0,
+        file_ignore_patterns = { "node_modules", ".git" },
+        mappings = {
+          i = {
+            ["<C-j>"] = require("telescope.actions").move_selection_next,
+            ["<C-k>"] = require("telescope.actions").move_selection_previous,
           },
         },
-    },
+      },
+      -- pickers = { ... }, -- Customize specific pickers
+      -- extensions = { ... }, -- Load extensions like fzf-native
+    }
+  end,
 }
