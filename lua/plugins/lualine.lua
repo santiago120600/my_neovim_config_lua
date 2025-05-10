cc0e7b750db1a9e557db6b62e9813a8a6f71426b
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-05-09 21:58:05 -0600

    Update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,30 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, {
-        function()
-          return ":v"
-        end,
-      })
-    end,
-}
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = "gruvbox", -- Or auto, or other themes
+        component_separators = "|",
+        section_separators = "",
+        -- ... other lualine options
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } }, -- Show relative path
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {},
+      },
+      -- ... other lualine sections/extensions
+    },
+  }
