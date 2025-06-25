6bc253ccfb5faf1d0ac5e922548eca4811199d43
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2025-06-25 16:20:47 -0600

    add autocmd to open telescope when opening a directory

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Open telescope when opening a directory",
+  pattern = "*",
+  callback = function(args)
+    -- Check if the argument is a directory (like ".")
+    local arg = args.file
+    if arg and vim.fn.isdirectory(arg) == 1 then
+      -- Load Telescope and open file browser
+      require("telescope.builtin").find_files({
+        cwd = arg,
+      })
+    end
+  end,
+})
